c54ca20809af6dfa2bdb1007b912655a2b6fbf4a
Quentin Deslandes <qde@naccy.de>
2024-10-10 17:52:39 +0200

    wip

---
+++ bench.cpp
@@ -0,0 +1,524 @@
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <stdexcept>
+#include <cerrno>
+#include <sstream>
+#include <iostream>
+#include <array>
+#include <unistd.h>
+#include <csignal>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <memory>
+#include <chrono>
+#include <thread>
+#include <bpf/bpf.h>
+#include <tuple>
+#include <fcntl.h>
+#include <benchmark/benchmark.h>
+#include <sys/syscall.h>      /* Definition of SYS_* constants */
+#include <unistd.h>
+
+#if defined(__i386__)
+#define _BF_NR_bpf 357
+#elif defined(__x86_64__)
+#define _BF_NR_bpf 321
+#elif defined(__aarch64__)
+#define _BF_NR_bpf 280
+#else
+#error _BF_NR_bpf not defined. bpfilter does not support your arch.
+#endif
+
+namespace bpfilter {
+
+int bpf(enum bpf_cmd cmd, union bpf_attr *attr)
+{
+    int r = (int)syscall(_BF_NR_bpf, cmd, attr, sizeof(*attr));
+    if (r < 0)
+        return -errno;
+
+    return r;
+}
+
+class BfDaemonOptions
+{
+public:
+    BfDaemonOptions &transient()
+    {
+        options_.push_back("--transient");
+        return *this;
+    }
+
+    BfDaemonOptions &noCli()
+    {
+        options_.push_back("--no-cli");
+        return *this;
+    }
+
+    BfDaemonOptions &noIptables()
+    {
+        options_.push_back("--no-iptables");
+        return *this;
+    }
+
+    BfDaemonOptions &noNftables()
+    {
+        options_.push_back("--no-nftables");
+        return *this;
+    }
+
+    BfDaemonOptions &bufferLen(::std::size_t len)
+    {
+        options_.push_back("--buffer-len");
+        options_.push_back(::std::to_string(len));
+        return *this;
+    }
+
+    BfDaemonOptions &verbose(const ::std::string &component)
+    {
+        options_.push_back("--verbose");
+        options_.push_back(component);
+        return *this;
+    }
+
+    ::std::string str() const
+    {
+        ::std::stringstream opts;
+
+        for (::std::size_t i = 0; i < options_.size(); ++i) {
+            opts << options_[i];
+            if (i != options_.size() - 1)
+                opts << " ";
+        }
+
+        return opts.str();
+    }
+
+    ::std::vector<::std::string> get() const
+    {
+        return options_;
+    }
+
+    ::std::vector<const char *> raw() const
+    {
+        ::std::vector<const char *> options;
+
+        for (const auto &opt: options_)
+            options.push_back(opt.c_str());
+
+        return options;
+    }
+
+private:
+    ::std::vector<::std::string> options_;
+};
+
+int execccc(::std::string bin, ::std::vector<::std::string> args, int *out = nullptr, int *err = nullptr)
+{
+    int stdout_pipe[2];
+    int stderr_pipe[2];
+    pid_t pid;
+
+    std::vector<const char *> args_;
+    args_.push_back(bin.c_str());
+    for (const auto &arg: args)
+        args_.push_back(arg.c_str());
+    args_.push_back(nullptr);
+
+    for (const auto &arg: args_) {
+        if (arg)
+            ::std::cout << "Arg: '" << arg << "'" << ::std::endl;
+        else
+            ::std::cout << "Arg: NULL" << ::std::endl;
+    }
+
+    if (pipe(stdout_pipe) != 0 || pipe(stderr_pipe) != 0)
+        throw ::std::invalid_argument("failed to create pipes");
+
+    pid = fork();
+    if (pid < 0)
+        throw ::std::invalid_argument("failed to fork");
+
+    if (pid == 0) {
+        dup2(stdout_pipe[1], STDOUT_FILENO);
+        dup2(stderr_pipe[1], STDERR_FILENO);
+
+        close(stdout_pipe[0]);
+        close(stderr_pipe[0]);
+
+        int rrr = execvp(bin.c_str(), (char *const *)(args_.data()));
+        ::std::cout << rrr << ::std::endl;
+        ::std::cout << geteuid() << ::std::endl;
+        throw ::std::invalid_argument("execvp failed: " + ::std::string(::std::strerror(errno)));
+    }
+
+    if (out)
+        *out = stdout_pipe[0];
+    else
+        close(stdout_pipe[0]);
+    if (err)
+        *err = stderr_pipe[0];
+    else
+        close(stderr_pipe[0]);
+
+    return pid;
+}
+
+::std::string read_fd(int fd)
+{
+    ssize_t len;
+    char buffer[1024] = {};
+    ::std::string data;
+
+
+    while ((len = read(fd, buffer, 1024)) >= 0) {
+        ::std::cout << "Read " << len << " bytes" << ::std::endl;
+        data += ::std::string(buffer, len);
+    }
+
+    if (len == 0)
+        ::std::cout << "EOF, all read!" << ::std::endl;
+    else if (len < 0)
+        ::std::cout << "GOT ERROR" << ::std::endl;
+
+    return data;
+}
+
+::std::tuple<::std::string, ::std::string> run(::std::string bin, ::std::vector<::std::string> args)
+{
+    int out, err;
+    ::std::cout << "execcc" << ::std::endl;
+    int fd = execccc(bin, args, &out, &err);
+    ::std::cout << "exec done" << ::std::endl;
+    ::std::string log, logerr;
+
+    int status;
+
+    char buffer[8192] = {};
+
+    int flags;
+    flags = fcntl(out, F_GETFL, 0);
+    fcntl(out, F_SETFL, flags | O_NONBLOCK);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    flags = fcntl(err, F_GETFL, 0);
+    fcntl(err, F_SETFL, flags | O_NONBLOCK);
+
+        ::std::cout << "read" << ::std::endl;
+    (void)waitpid(fd, &status, 0);
+        ::std::cout << "wait pid done" << ::std::endl;
+
+    return {read_fd(out), read_fd(err)};
+}
+
+class BfDaemon
+{
+public:
+    BfDaemon(const ::std::string &path = "bpfilter", BfDaemonOptions options = BfDaemonOptions()):
+        path_{path},
+        options_{options}
+    {
+
+    }
+
+    void start()
+    {
+        ::std::cout << path_ << ::std::endl;
+        pid_ = execccc(path_, options_.get(), &out, &err);
+        int flags = fcntl(out, F_GETFL, 0);
+        fcntl(out, F_SETFL, flags | O_NONBLOCK);
+        flags = fcntl(err, F_GETFL, 0);
+        fcntl(err, F_SETFL, flags | O_NONBLOCK);
+
+        while (true) {
+            std::string data = read_fd(err);
+            if (data.find("waiting for requests...") != std::string::npos) {
+                std::cout << "Daemon is ready!" << '\n';
+                break;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+            ::std::cout << "not found: " << data << ::std::endl;
+        }
+    }
+
+    void stop()
+    {
+        int r = kill(pid_, SIGINT);
+        if (r < 0)
+            throw ::std::runtime_error("failed to send SIGINT " + ::std::string(::std::strerror(errno)));
+
+        int status;
+        (void)waitpid(pid_, &status, 0);
+
+        ::std::cout << read_fd(out) << ::std::endl;
+        ::std::cout << read_fd(err) << ::std::endl;
+
+        //pclose(process_);
+    }
+
+private:
+    ::std::string path_;
+    BfDaemonOptions options_;
+    FILE *process_ = nullptr;
+    pid_t pid_;
+    int out;
+    int err;
+
+    ::std::string buildCmd() const
+    {
+        return path_ + " " + options_.str();
+    }
+};
+
+class BfChain
+{
+public:
+    BfChain() {}
+
+    ~BfChain()
+    {
+        cleanup();
+    }
+
+    BfChain(BfChain &other) = delete;
+    BfChain(BfChain &&other) = delete;
+    BfChain &operator=(BfChain &other) = delete;
+    BfChain &operator=(BfChain &&other) = delete;
+
+    BfChain& operator<<(const ::std::string &rule) {
+        m_rules.push_back(rule);
+        return *this;
+    }
+
+    BfChain& repeatRule(const ::std::string &rule, ::std::size_t count) {
+        for (::std::size_t i = 0; i < count; ++i)
+        m_rules.push_back(rule);
+
+        return *this;
+    }
+
+    int fd() const
+    {
+        return fd_;
+    }
+
+    int init()
+    {
+        if (fd_ == -1)
+        close();
+
+        int r = apply();
+        if (r < 0)
+        return r;
+
+        return open();
+    }
+
+    void cleanup()
+    {
+        close();
+    }
+
+private:
+    ::std::vector<::std::string> m_rules;
+    int fd_ = -1;
+
+    ::std::string ruleset() const
+    {
+        ::std::string chain = "chain BF_HOOK_CGROUP_INGRESS{cgroup=invalid,attach=no,name=bf_benchmark} policy ACCEPT ";
+
+        for (const auto &s: m_rules)
+        chain += (s + " ");
+
+        return chain;
+    }
+
+    int apply() const
+    {
+
+        ::std::vector<::std::string> args;
+        args.push_back("--str");
+        args.push_back("chain BF_HOOK_XDP{ifindex=1,name=bf_benchmark,attach=no} policy ACCEPT rule meta.l4_proto tcp counter ACCEPT");
+        const auto [out, err] = run("/home/quentin/Projects/bpfilter/build/output/bin/bfcli", args);
+        ::std::cout << "run done" << ::std::endl;
+        ::std::cout << "out: " << out << ::std::endl;
+        ::std::cout << "err: " << err << ::std::endl;
+
+        return 0;
+    }
+
+    int open()
+    {
+        uint32_t id = 0;
+        int r;
+
+        while (true) {
+            r = bpf_prog_get_next_id(id, &id);
+            if (r < 0) {
+                ::std::cerr << "call to bpf_prog_get_next_id() failed: " << ::strerror(r) << ::std::endl;
+                return r;
+            }
+
+            int prog_fd = bpf_prog_get_fd_by_id(id);
+            if (prog_fd < 0) {
+                ::std::cerr << "call to bpf_prog_get_fd_by_id() failed: " << ::strerror(prog_fd) << ::std::endl;
+                return prog_fd;
+            }
+
+            struct bpf_prog_info info = {};
+            uint32_t len = sizeof(info);
+            r = bpf_prog_get_info_by_fd(prog_fd, &info, &len);
+            if (r < 0) {
+                ::close(prog_fd);
+                ::std::cerr << "call to bpf_prog_get_info_by_fd() failed: " << ::strerror(r) << ::std::endl;
+                return r;
+            }
+
+            if (::std::string(info.name) == ::std::string("bf_benchmark")) {
+                fd_ = prog_fd;
+                return 0;
+            }
+
+            ::close(prog_fd);
+        }
+
+        return -ENOENT;
+    }
+
+    void close()
+    {
+        if (fd_ == -1)
+        return;
+
+        ::close(fd_);
+        fd_ = -1;
+    }
+};
+
+class Program
+{
+public:
+    Program(std::string name): name_{name}
+    {
+
+    }
+
+    int open()
+    {
+        uint32_t id = 0;
+        int r;
+
+        while (true) {
+            r = bpf_prog_get_next_id(id, &id);
+            if (r < 0) {
+                ::std::cerr << "call to bpf_prog_get_next_id() failed: " << ::strerror(r) << ::std::endl;
+                return r;
+            }
+
+            int prog_fd = bpf_prog_get_fd_by_id(id);
+            if (prog_fd < 0) {
+                ::std::cerr << "call to bpf_prog_get_fd_by_id() failed: " << ::strerror(prog_fd) << ::std::endl;
+                return prog_fd;
+            }
+
+            struct bpf_prog_info info = {};
+            uint32_t len = sizeof(info);
+            r = bpf_prog_get_info_by_fd(prog_fd, &info, &len);
+            if (r < 0) {
+                ::close(prog_fd);
+                ::std::cerr << "call to bpf_prog_get_info_by_fd() failed: " << ::strerror(r) << ::std::endl;
+                return r;
+            }
+
+            if (::std::string(info.name) == name_) {
+                fd_ = prog_fd;
+                return 0;
+            }
+
+            ::close(prog_fd);
+        }
+
+        return 0;
+    }
+
+    void close()
+    {
+        ::close(fd_);
+        fd_ = -1;
+    }
+
+    int run(int expect, int iters, void *pkt, int len)
+    {
+        LIBBPF_OPTS(
+            bpf_test_run_opts,
+            opts,
+            .data_in = (const void*)pkt,
+            .data_size_in = len,
+            .repeat = (int)iters
+        );
+
+        int r = bpf_prog_test_run_opts(fd_, &opts);
+        if (r < 0){
+            ::std::cout << "Failed to run prog" << ::std::endl;
+            return r;
+            }
+
+        return 0;
+    }
+
+private:
+    ::std::string name_;
+    int fd_ = -1;
+};
+
+}
+
+constexpr uint8_t kLocalhostIPv6TCPPkt[] = {
+
+    0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x86, 0xdd, 0x60, 0x00, 0x00, 0x00, 0x00, 0x14, 0x06, 0x40,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x7a,
+    0x69, 0x7a, 0xb7, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x50, 0x02, 0x20, 0x00, 0x9a, 0xbf, 0x00, 0x0};
+
+static void BM_test(benchmark::State& state) {
+    ::bpfilter::Program p("bf_benchmark");
+    p.open();
+
+    for (auto _ : state) {
+        p.run(2, 1, (void *)kLocalhostIPv6TCPPkt, sizeof(kLocalhostIPv6TCPPkt));
+    }
+}
+BENCHMARK(BM_test);
+
+int main(int argc, char* argv[])
+{
+    if (geteuid() != 0)
+        throw std::runtime_error("the benchmark should be run as root");
+
+    auto d = bpfilter::BfDaemon(
+        "/home/quentin/Projects/bpfilter/build/output/bin/bpfilter",
+        bpfilter::BfDaemonOptions().transient().noIptables().noNftables()
+    );
+
+    d.start();
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    ::bpfilter::BfChain chain;
+    chain.init();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+   ::benchmark::Initialize(&argc, argv);
+   ::benchmark::RunSpecifiedBenchmarks();
+
+    chain.cleanup();
+    d.stop();
+
+    return 0;
+}
